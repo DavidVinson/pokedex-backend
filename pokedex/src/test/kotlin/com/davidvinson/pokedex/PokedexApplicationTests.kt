fdb2d4b34632a8b17f97f77e69f689b7d4acbf00
David Vinson <dvinson@bushelpowered.com>
2021-10-15 13:06:16 -0500

    initial commit

---
+++ pokedex/src/test/kotlin/com/davidvinson/pokedex/PokedexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.davidvinson.pokedex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
