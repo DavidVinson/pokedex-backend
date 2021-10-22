8b22b0c0beb061f043b07ed945ecda0eea154f5d
David Vinson <dvinson@bushelpowered.com>
2021-10-22 12:41:08 -0500

    added ResponseEntity to controller

--- pokedex/src/main/kotlin/com/davidvinson/pokedex/controller/PokemonController.kt
+++ pokedex/src/main/kotlin/com/davidvinson/pokedex/controller/PokemonController.kt
@@ -2,6 +2,7 @@ package com.davidvinson.pokedex.controller
 
 import com.davidvinson.pokedex.model.*
 import com.davidvinson.pokedex.service.PokemonService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -10,24 +11,21 @@ import org.springframework.web.bind.annotation.RestController
 class PokemonController(val pokemonService: PokemonService) {
 
     @GetMapping("/api/pokemon")
-    fun getAllPokemon(): List<PokemonListResponse> {
-        return pokemonService.getAllPokemon().map {pokemon -> pokemon.toListResponse()}
+    fun getAllPokemon(): ResponseEntity<List<PokemonListResponse>> {
+        return ResponseEntity.ok(pokemonService.getAllPokemon().map {pokemon -> pokemon.toListResponse()})
     }
 
     @GetMapping("/api/pokemon/{id}")
-    fun getPokemonById(@PathVariable("id") pokemonId: Int): PokemonResponse {
-        return pokemonService.getPokemonById(pokemonId).toResponse()
+    fun getPokemonById(@PathVariable("id") pokemonId: Int): ResponseEntity<PokemonResponse> {
+        return ResponseEntity.ok(pokemonService.getPokemonById(pokemonId).toResponse())
 
     }
 
 //    @GetMapping("/api/pokemon/{id}")
-//    fun getPokemonById(@PathVariable("id") pokemonId: Int): ResponseEntity<PokemonResponse> {
-//       return ResponseEntity<PokemonResponse>(HttpStatus.FOUND).statusCode
+//    fun getPokemonById(@PathVariable("id") pokemonId: Int): PokemonResponse {
+//        return pokemonService.getPokemonById(pokemonId).toResponse()
 //
 //    }
-
-//    @GetMapping("/api/pokemon/{name}")
-//    fun getPokemonByName(@RequestParam("name") name: String): Pokemon = pokemonService.getPokemonName(name)
 
 }
 
