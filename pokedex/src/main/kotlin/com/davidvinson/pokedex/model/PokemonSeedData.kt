a203bf896bd68aa0b80cb05af4d826ffb465107e
David Vinson <dvinson@bushelpowered.com>
2021-10-29 16:57:08 -0500

    nothing to show for today

--- pokedex/src/main/kotlin/com/davidvinson/pokedex/model/PokemonSeedData.kt
+++ pokedex/src/main/kotlin/com/davidvinson/pokedex/model/PokemonSeedData.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import javax.persistence.*
 
+
 @Service
 class PokemonSeedDataService(
     val pokemonDataRepository: PokemonDataRepository,
@@ -28,6 +29,13 @@ class PokemonSeedDataService(
                 val description: String? = row["description"]
                 val types: String? = row["types"]
 
+                //clean types string by removing "[]" from the string
+                val newTypes: String = types!!.drop(1).dropLast(1)
+                //clean types by creating a list of types
+                val newTypes2: List<String> = newTypes.split(",")
+                //create a unique set of pokemon types
+                newTypes2.forEach { type -> newTypesSet.add(type.trim())}
+
                 val pokemon: PokemonDataEntity = PokemonDataEntity(
 //                    id = id?.toInt(),
                     pokeName = name,
@@ -35,20 +43,10 @@ class PokemonSeedDataService(
                     weight = weight?.toDouble(),
                     genus = genus,
                     description = description,
-                    types =
                 )
-
-                //clean types string by removing "[]" from the string
-                val newTypes: String = types!!.drop(1).dropLast(1)
-                //clean types by creating a list of types
-                val newTypes2: List<String> = newTypes.split(",")
-                //create a unique set of pokemon types
-                newTypes2.forEach { it -> newTypesSet.add(it.trim())}
 
                 saveToDB(pokemon)
             }
-
-
         }
         //loop through newTypesSet and create entity to be saved to db
         newTypesSet.forEach { item ->
@@ -57,8 +55,6 @@ class PokemonSeedDataService(
             )
             saveTypeToDB(pokemonType)
         }
-
-//        println(newTypesSet.size)
 
     }
 
@@ -70,13 +66,15 @@ class PokemonSeedDataService(
         return pokemonDataTypeRepository.save(pokemonType)
     }
 
+
+
     } // End PokemonSeedDataService
 
 
 
 
 @Entity
-@Table(name = "pokemonData")
+@Table(name = "pokemonData1")
 class PokemonDataEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -87,20 +85,36 @@ class PokemonDataEntity(
     val genus: String?,
     val description: String?,
 
-    @ManyToMany
-    val types: List<PokemonDataTypeEntity>
+    @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.PERSIST])
+    @JoinTable(
+        name = "pokemonData1_pokemonDataTypes1",
+        joinColumns = [
+            JoinColumn(name = "pokemon_Data1_id")
+        ],
+        inverseJoinColumns = [
+            JoinColumn(name = "pokemon_data_types1_id")
+        ]
+    )
+    val types: List<String> = listOf()
+
 
 )
 
 @Entity
-@Table(name = "pokemonDataType")
+@Table(name = "pokemonDataType1")
 class PokemonDataTypeEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
-    val name: String?
+    val name: String?,
 
-)
+    @ManyToMany(mappedBy = "types", cascade = [CascadeType.PERSIST], fetch = FetchType.EAGER)
+    val pokemons: MutableList<PokemonDataEntity> = mutableListOf()
+) {
+    fun addPokemon(pokemon: PokemonDataEntity) {
+        pokemons.add(pokemon)
+    }
+}
 
 @Repository
 interface PokemonDataRepository : JpaRepository<PokemonDataEntity, String> {}
@@ -108,7 +122,67 @@ interface PokemonDataRepository : JpaRepository<PokemonDataEntity, String> {}
 @Repository
 interface PokemonDataTypeRepository : JpaRepository<PokemonDataTypeEntity, String> {}
 
+/*
+@Entity
+@Table(name = "SUPER_HERO")
+class SuperHero {
+    @Id
+    @GeneratedValue
+    var id: Int? = null
+    var name: String? = null
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.PERSIST])
+    @JoinTable(
+        name = "SuperHero_Movies",
+        joinColumns = [JoinColumn(name = "superhero_id")],
+        inverseJoinColumns = [JoinColumn(name = "movie_id")]
+    )
+    private val movies: Set<Movie> = HashSet<Movie>()
 
+    constructor() {}
+    constructor(id: Int?, name: String?) {
+        this.id = id
+        this.name = name
+    }
 
+    constructor(name: String?) {
+        this.name = name
+    }
 
+    fun getMovies(): Set<Movie> {
+        return movies
+    }
+
+}
+
+@Entity
+@Table(name = "MOVIE")
+class Movie {
+    @Id
+    @GeneratedValue
+    var id: Int? = null
+    var title: String? = null
+
+    @ManyToMany(mappedBy = "movies", cascade = [CascadeType.PERSIST], fetch = FetchType.EAGER)
+    private val superHeroes: MutableSet<SuperHero> = HashSet()
+
+    constructor() {}
+    constructor(id: Int?, title: String?) {
+        this.id = id
+        this.title = title
+    }
+
+    constructor(title: String?) {
+        this.title = title
+    }
+
+    fun getSuperHeroes(): Set<SuperHero> {
+        return superHeroes
+    }
+
+    fun addSuperHero(superHero: SuperHero) {
+        superHeroes.add(superHero)
+        superHero.getMovies().add(this)
+    }
+}
+*/
